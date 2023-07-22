6ccbcf51c7733b8aa2a0b714fe01cb9f07b1a46a
shmsacr <shmsacr@gmail.com>
2023-07-23 00:08:46 +0300

    feat: added search button and add button to appbar

--- lib/view/screens/home/home_screen.dart
+++ lib/view/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lingo/view/screens/home/add_word_screen.dart';
 
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({
@@ -8,8 +9,25 @@ class HomeScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Center(
-      child: Text("HomeScreen"),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0x5F5F5F5F),
+        title: Text("LİNGO", textAlign: TextAlign.center),
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => AddWord()));
+          },
+          icon: Icon(Icons.add),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.search),
+          ),
+        ],
+      ),
     );
   }
 }
