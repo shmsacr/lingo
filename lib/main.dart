8fa3f03ed99991a5d3a0ec80b8460d63ab8d6056
shmsacr <shmsacr@gmail.com>
2023-07-23 22:41:38 +0300

    feat: initialize hive package

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:get_it/get_it.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:lingo/data/services/local_storage.dart';
 import 'package:lingo/view/screens/dashboard.dart';
 import 'package:lingo/view/theme/app_theme.dart';
 
-void main() {
+import 'data/model/word_model.dart';
+
+final locator = GetIt.instance;
+void setup() {
+  locator.registerSingleton<LocalStroge>(HiveLocalStroge());
+}
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  Hive.registerAdapter(WordsAdapter());
+  setup();
   runApp(const ProviderScope(child: MyApp()));
 }
 
@@ -14,7 +28,7 @@ class MyApp extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return MaterialApp(      
+    return MaterialApp(
       theme: AppTheme.light.copyWith(
         scaffoldBackgroundColor: Colors.white,
       ),
