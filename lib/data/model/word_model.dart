7dc5c7d4a424f6ba2a7aa45b5d5e432d2d22c29e
shmsacr <shmsacr@gmail.com>
2023-07-23 22:42:44 +0300

    feat: add words model

---
+++ lib/data/model/word_model.dart
@@ -0,0 +1,22 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:hive/hive.dart';
+
+part 'word_model.freezed.dart';
+part 'word_model.g.dart';
+
+@freezed
+@HiveType(typeId: 0)
+class Words with _$Words {
+  @JsonSerializable(explicitToJson: true)
+  @HiveField(0)
+  const factory Words({
+    @HiveField(1) required final String id,
+    @HiveField(2) required final String word,
+    @HiveField(3) required final String means,
+    @HiveField(4) final String? spouse,
+    @HiveField(5) final String? sentence,
+    @Default(false) @HiveField(6) final bool isComplete,
+  }) = _Words;
+
+  factory Words.fromJson(Map<String, Object?> json) => _$WordsFromJson(json);
+}
