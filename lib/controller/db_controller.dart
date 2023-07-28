a14e9d5a6364266fc010016619ef36f60a4b08ab
shmsacr <shmsacr@gmail.com>
2023-07-28 18:38:43 +0300

    feat: create database controller

---
+++ lib/controller/db_controller.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../data/model/word_model.dart';
+import '../data/services/local_storage.dart';
+
+final databaseProvider = Provider((ref) => HiveLocalStroge());
+final wordsProvider = FutureProvider<List<Words>>((ref) async {
+  final dbProvider = ref.read(databaseProvider);
+  return await dbProvider.getAllWords();
+});
