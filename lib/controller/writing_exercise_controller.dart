a770d132517b769ae17f03c36a81bd19d79cebdd
shmsacr <shmsacr@gmail.com>
2023-07-31 15:23:16 +0300

    feat: update fillcolorprovider and randemindexprovider

--- lib/controller/writing_exercise_controller.dart
+++ lib/controller/writing_exercise_controller.dart
@@ -6,8 +6,6 @@ import 'package:lingo/data/model/word_model.dart';
 
 import 'db_controller.dart';
 
-final userInputProvider = StateProvider<String>((ref) => '');
-
 final randomWordProvider = FutureProvider<Words>((ref) async {
   final wordList = await ref.read(wordsProvider.future);
   final randomIndex = Random().nextInt(wordList.length);
@@ -16,10 +14,5 @@ final randomWordProvider = FutureProvider<Words>((ref) async {
   return wordList[randomIndex];
 });
 
-final fillColorProvider =
-    StateProvider.autoDispose<Color>((ref) => Colors.blue.shade100);
-final randomIndexProvider =
-    StateProvider.autoDispose<int>((ref) => Random().nextInt(1));
-
-final userTextProvider =
-    StateProvider<TextEditingController>((ref) => TextEditingController());
+final fillColorProvider = StateProvider<Color>((ref) => Colors.blueGrey);
+final randomIndexProvider = StateProvider<int>((ref) => Random().nextInt(1));
