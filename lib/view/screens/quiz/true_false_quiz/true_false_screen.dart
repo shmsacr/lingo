351c0c912a33456ee0c703cd18934b5e36af1db3
shmsacr <shmsacr@gmail.com>
2023-08-01 07:42:53 +0300

    feat: init true false quiz

---
+++ lib/view/screens/quiz/true_false_quiz/true_false_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class TrueFalseScreen extends ConsumerWidget {
+  const TrueFalseScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container();
+  }
+}
