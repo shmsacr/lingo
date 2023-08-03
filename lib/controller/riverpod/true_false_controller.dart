73e3479312dd87f1bc79f70471a23875894bc08e
shmsacr <shmsacr@gmail.com>
2023-08-03 23:02:29 +0300

    feat: add statenotiferprovider to futureprovider

--- lib/controller/riverpod/true_false_controller.dart
+++ lib/controller/riverpod/true_false_controller.dart
@@ -4,42 +4,29 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lingo/controller/riverpod/words_controller.dart';
 import 'package:lingo/data/model/true_false_model.dart';
 
-import '../../data/model/word_model.dart';
-
-final trueFalseProvider =
-    StateNotifierProvider<TrueFalseNotifier, List<TrueFalse>>((ref) {
-  final allWords = ref.watch(wordListNotifierProvider);
-  return TrueFalseNotifier(allWord: allWords);
+final trueFalseProvider = FutureProvider<TrueFalse>((ref) async {
+  final allWord = ref.watch(wordListNotifierProvider);
+  int randomIndex = Random().nextInt(allWord.length);
+  print(randomIndex);
+  int randomIndex1 = Random().nextInt(2);
+  print(randomIndex1);
+  if (randomIndex1 == 1) {
+    TrueFalse trueFalse = await TrueFalse(
+        word: allWord[randomIndex].word,
+        means: allWord[randomIndex].means,
+        result: true);
+    print(trueFalse.means);
+    return trueFalse;
+  } else {
+    int randomIndex3;
+    do {
+      randomIndex3 = await Random().nextInt(allWord.length);
+    } while (randomIndex == randomIndex3);
+    TrueFalse trueFalse = await TrueFalse(
+        word: allWord[randomIndex].word,
+        means: allWord[randomIndex3].means,
+        result: false);
+    print(trueFalse.means);
+    return trueFalse;
+  }
 });
-
-class TrueFalseNotifier extends StateNotifier<List<TrueFalse>> {
-  TrueFalseNotifier({required this.allWord}) : super([]) {
-    trueFalseList();
-  }
-
-  List<Words> allWord;
-
-  Future<void> trueFalseList() async {
-    int randomIndex = Random().nextInt(allWord.length + 1);
-    int randomIndex1 = Random().nextInt(2);
-    if (randomIndex1 == 1) {
-      TrueFalse trueFalse = TrueFalse(
-          word: allWord[randomIndex].word,
-          means: allWord[randomIndex].means,
-          result: true);
-      print(trueFalse.means);
-      state = [trueFalse];
-    } else {
-      int randomIndex3;
-      do {
-        randomIndex3 = Random().nextInt(allWord.length + 1);
-      } while (randomIndex == randomIndex3);
-      TrueFalse trueFalse = TrueFalse(
-          word: allWord[randomIndex].word,
-          means: allWord[randomIndex3].means,
-          result: false);
-      print(trueFalse.means);
-      state = [trueFalse];
-    }
-  }
-}
