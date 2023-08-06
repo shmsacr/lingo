a2d4ecf2a64bbc63a8b2fbd169e350e2cfd44532
shmsacr <shmsacr@gmail.com>
2023-08-06 23:28:20 +0300

    feat: add theme change

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:get_it/get_it.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:lingo/data/services/local_storage.dart';
 import 'package:lingo/controller/routes/router.dart' as route;
+import 'package:lingo/data/services/local_storage.dart';
+import 'package:lingo/view/theme/app_theme.dart';
 
 //D:\FlutterWork\lingo\lib\controller\routes\router.dart
 import 'data/model/word_model.dart';
-import 'view/theme/custom_theme.dart';
 
 final locator = GetIt.instance;
 void setup() {
@@ -32,8 +32,8 @@ class MyApp extends ConsumerWidget {
     return MaterialApp(
       initialRoute: route.Router.dashboard,
       onGenerateRoute: route.Router.generateRoute,
-      theme: CustomAppTheme().theme,
-      themeMode: ThemeMode.system,
+      theme: AppTheme.light,
+      darkTheme: AppTheme.dark,
       debugShowCheckedModeBanner: false,
     );
   }
