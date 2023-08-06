3500ae47ed9263812120c883c51915c44fb9b94b
shmsacr <shmsacr@gmail.com>
2023-08-06 23:29:01 +0300

    feat: add flex color scheme package

--- lib/view/theme/app_theme.dart
+++ lib/view/theme/app_theme.dart
@@ -33,28 +33,92 @@
 //     tones: FlexTones.vividSurfaces(Brightness.light),
 //     visualDensity: FlexColorScheme.comfortablePlatformDensity,
 //     useMaterial3: true,
- 
-//     fontFamily: GoogleFonts.poppins().fontFamily,
-//   );
-//   static final dark = FlexThemeData.dark(
-//     colors: const FlexSchemeColor(
-//       primary: AppColors.primary,
-//       primaryContainer: AppColors.primaryContainer,
-//       secondary: AppColors.secondary,
-//       secondaryContainer: AppColors.secondaryContainer,
-//       tertiary: AppColors.tertiary,
-//       tertiaryContainer: AppColors.tertiaryContainer,
-//       appBarColor: AppColors.appBarColor,
-//       error: AppColors.error,
-//     ),
-//     surfaceMode: FlexSurfaceMode.highScaffoldLowSurface,
-//     blendLevel: 15,
-//     appBarStyle: FlexAppBarStyle.background,
-//     appBarOpacity: 0.90,
-//     subThemesData: const FlexSubThemesData(
-//       blendOnLevel: 30,
-//     ),
-//     visualDensity: FlexColorScheme.comfortablePlatformDensity,
-//     useMaterial3: true,
-//   );
-// }
+
+import 'package:flex_color_scheme/flex_color_scheme.dart';
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final light = FlexThemeData.light(
+    textTheme: TextTheme(titleMedium: TextStyle(color: Colors.yellow)),
+    scheme: FlexScheme.materialBaseline,
+    subThemesData: const FlexSubThemesData(
+      interactionEffects: false,
+      tintedDisabledControls: false,
+      blendOnColors: false,
+      useTextTheme: true,
+      inputDecoratorBorderType: FlexInputBorderType.underline,
+      inputDecoratorUnfocusedBorderIsColored: false,
+      tooltipRadius: 4,
+      tooltipSchemeColor: SchemeColor.inverseSurface,
+      tooltipOpacity: 0.9,
+      snackBarElevation: 6,
+      snackBarBackgroundSchemeColor: SchemeColor.inverseSurface,
+      navigationBarSelectedLabelSchemeColor: SchemeColor.onSurface,
+      navigationBarUnselectedLabelSchemeColor: SchemeColor.onSurface,
+      navigationBarMutedUnselectedLabel: false,
+      navigationBarSelectedIconSchemeColor: SchemeColor.onSurface,
+      navigationBarUnselectedIconSchemeColor: SchemeColor.onSurface,
+      navigationBarMutedUnselectedIcon: false,
+      navigationBarIndicatorSchemeColor: SchemeColor.secondaryContainer,
+      navigationBarIndicatorOpacity: 1.00,
+      navigationRailSelectedLabelSchemeColor: SchemeColor.onSurface,
+      navigationRailUnselectedLabelSchemeColor: SchemeColor.onSurface,
+      navigationRailMutedUnselectedLabel: false,
+      navigationRailSelectedIconSchemeColor: SchemeColor.onSurface,
+      navigationRailUnselectedIconSchemeColor: SchemeColor.onSurface,
+      navigationRailMutedUnselectedIcon: false,
+      navigationRailIndicatorSchemeColor: SchemeColor.secondaryContainer,
+      navigationRailIndicatorOpacity: 1.00,
+      navigationRailBackgroundSchemeColor: SchemeColor.surface,
+      navigationRailLabelType: NavigationRailLabelType.none,
+    ),
+    keyColors: const FlexKeyColors(),
+    visualDensity: FlexColorScheme.comfortablePlatformDensity,
+    useMaterial3: true,
+    swapLegacyOnMaterial3: true,
+    // To use the Playground font, add GoogleFonts package and uncomment
+    // fontFamily: GoogleFonts.notoSans().fontFamily,
+  );
+  static final dark = FlexThemeData.dark(
+    textTheme: TextTheme()
+        .apply(bodyColor: Colors.yellow, displayColor: Colors.yellow),
+
+    scheme: FlexScheme.materialBaseline,
+    subThemesData: const FlexSubThemesData(
+      interactionEffects: false,
+      tintedDisabledControls: false,
+      useTextTheme: true,
+      inputDecoratorBorderType: FlexInputBorderType.underline,
+      inputDecoratorUnfocusedBorderIsColored: false,
+      tooltipRadius: 4,
+      tooltipSchemeColor: SchemeColor.inverseSurface,
+      tooltipOpacity: 0.9,
+      snackBarElevation: 6,
+      snackBarBackgroundSchemeColor: SchemeColor.inverseSurface,
+      navigationBarSelectedLabelSchemeColor: SchemeColor.onSurface,
+      navigationBarUnselectedLabelSchemeColor: SchemeColor.onSurface,
+      navigationBarMutedUnselectedLabel: false,
+      navigationBarSelectedIconSchemeColor: SchemeColor.onSurface,
+      navigationBarUnselectedIconSchemeColor: SchemeColor.onSurface,
+      navigationBarMutedUnselectedIcon: false,
+      navigationBarIndicatorSchemeColor: SchemeColor.secondaryContainer,
+      navigationBarIndicatorOpacity: 1.00,
+      navigationRailSelectedLabelSchemeColor: SchemeColor.onSurface,
+      navigationRailUnselectedLabelSchemeColor: SchemeColor.onSurface,
+      navigationRailMutedUnselectedLabel: false,
+      navigationRailSelectedIconSchemeColor: SchemeColor.onSurface,
+      navigationRailUnselectedIconSchemeColor: SchemeColor.onSurface,
+      navigationRailMutedUnselectedIcon: false,
+      navigationRailIndicatorSchemeColor: SchemeColor.secondaryContainer,
+      navigationRailIndicatorOpacity: 1.00,
+      navigationRailBackgroundSchemeColor: SchemeColor.surface,
+      navigationRailLabelType: NavigationRailLabelType.none,
+    ),
+    keyColors: const FlexKeyColors(),
+    visualDensity: FlexColorScheme.comfortablePlatformDensity,
+    useMaterial3: true,
+    swapLegacyOnMaterial3: true,
+    // To use the Playground font, add GoogleFonts package and uncomment
+    // fontFamily: GoogleFonts.notoSans().fontFamily,
+  );
+}
