71d4b27d5fd35db2ef657e4f893440e245075995
shmsacr <shmsacr@gmail.com>
2023-08-25 17:47:58 +0300

    feat: change means type

--- lib/data/model/true_false_model.dart
+++ lib/data/model/true_false_model.dart
@@ -3,12 +3,12 @@ import 'package:flutter/cupertino.dart';
 @immutable
 class TrueFalse {
   final String word;
-  final String means;
-  final bool result;
+  final List<String> means;
+  bool result;
 
   TrueFalse({required this.word, required this.means, required this.result});
 
-  TrueFalse copyWith({String? word, String? means, bool? result}) {
+  TrueFalse copyWith({String? word, List<String>? means, bool? result}) {
     return TrueFalse(
         word: word ?? this.word,
         means: means ?? this.means,
