6ad11ded5260714a4e80cd56676ea0a27b066768
shmsacr <shmsacr@gmail.com>
2023-08-26 22:58:00 +0300

    feat: add delete and add provider

--- lib/controller/riverpod/db_controller.dart
+++ lib/controller/riverpod/db_controller.dart
@@ -8,3 +8,16 @@ final wordsProvider = FutureProvider<List<Words>>((ref) async {
   final dbProvider = ref.read(databaseProvider);
   return await dbProvider.getAllWords();
 });
+
+final deleteWordProvider =
+    FutureProvider.family<void, Words>((ref, word) async {
+  final dbProvider = ref.read(databaseProvider);
+  await dbProvider.deleteWord(word: word);
+  ref.invalidate(wordsProvider);
+});
+
+final addWordProvider = FutureProvider.family<void, Words>((ref, word) async {
+  final dbProvider = ref.read(databaseProvider);
+  await dbProvider.addWord(word: word);
+  ref.invalidate(wordsProvider);
+});
