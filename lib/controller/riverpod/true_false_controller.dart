cd31a2ed6987200354a610a73a1c5a4fe0703965
shmsacr <shmsacr@gmail.com>
2023-08-26 23:00:32 +0300

    feat: change provider

--- lib/controller/riverpod/true_false_controller.dart
+++ lib/controller/riverpod/true_false_controller.dart
@@ -2,14 +2,15 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:lingo/controller/riverpod/words_controller.dart';
 import 'package:lingo/data/model/true_false_model.dart';
 import 'package:lingo/view/theme/app_colors.dart';
 
-final trueFalseProvider = FutureProvider<TrueFalse>((ref) async {
-  final allWord = ref.watch(wordListNotifierProvider);
+import '../../../controller/riverpod/db_controller.dart';
 
-  int randomIndex = Random().nextInt(allWord.length);
+final trueFalseProvider = FutureProvider<TrueFalse>((ref) async {
+  final allWord = ref.watch(wordsProvider);
+
+  int randomIndex = Random().nextInt(allWord.value!.length);
 
   print(randomIndex);
 
@@ -19,20 +20,23 @@ final trueFalseProvider = FutureProvider<TrueFalse>((ref) async {
 
   if (randomIndex1 == 1) {
     TrueFalse trueFalse = await TrueFalse(
-        word: allWord[randomIndex].word,
-        means: [allWord[randomIndex].means],
+        word: allWord.value![randomIndex].word,
+        means: [allWord.value![randomIndex].means],
         result: true);
     print(trueFalse.means);
     return trueFalse;
   } else {
     int randomIndex3;
     do {
-      randomIndex3 = await Random().nextInt(allWord.length);
+      randomIndex3 = await Random().nextInt(allWord.value!.length);
     } while (randomIndex == randomIndex3);
 
     TrueFalse trueFalse = await TrueFalse(
-        word: allWord[randomIndex].word,
-        means: [allWord[randomIndex3].means, allWord[randomIndex].means],
+        word: allWord.value![randomIndex].word,
+        means: [
+          allWord.value![randomIndex3].means,
+          allWord.value![randomIndex].means
+        ],
         result: false);
     print(trueFalse.means);
     return trueFalse;
