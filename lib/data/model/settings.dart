47d9fb174f629e53e4d423e4618e4cb4c9f7dcf4
shmsacr <shmsacr@gmail.com>
2023-08-31 20:37:45 +0300

    feat: add words list sorting and layout model

--- lib/data/model/settings.dart
+++ lib/data/model/settings.dart
@@ -5,14 +5,21 @@ part 'settings.g.dart';
 @HiveType(typeId: 1)
 class Settings extends HiveObject {
   // ignore: invalid_annotation_target
-  @HiveField(0, defaultValue: false)
+  @HiveField(0)
   final bool themeMode;
-  @HiveField(1, defaultValue: true)
+  @HiveField(1)
   final bool soundOn;
-  @HiveField(2, defaultValue: false)
+  @HiveField(2)
   final bool notification;
-  @HiveField(3, defaultValue: "mid")
-  final String fontSize;
+  @HiveField(3)
+  final int fontSize;
+  @HiveField(4)
+  final int sorting;
 
-  Settings(this.themeMode, this.soundOn, this.notification, this.fontSize);
+  Settings(
+      {required this.themeMode,
+      required this.soundOn,
+      required this.notification,
+      required this.fontSize,
+      required this.sorting});
 }
