c89191181b6dbb2df90e36df771a55806377a04a
shmsacr <shmsacr@gmail.com>
2023-09-01 20:47:10 +0300

    feat: delete sound data

--- lib/data/model/settings.dart
+++ lib/data/model/settings.dart
@@ -8,17 +8,14 @@ class Settings extends HiveObject {
   @HiveField(0)
   final bool themeMode;
   @HiveField(1)
-  final bool soundOn;
-  @HiveField(2)
   final bool notification;
-  @HiveField(3)
+  @HiveField(2)
   final int fontSize;
-  @HiveField(4)
+  @HiveField(3)
   final int sorting;
 
   Settings(
       {required this.themeMode,
-      required this.soundOn,
       required this.notification,
       required this.fontSize,
       required this.sorting});
