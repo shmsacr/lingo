9f5708e4a198af3acd67b3faf98df0da92e1628d
shmsacr <shmsacr@gmail.com>
2023-09-08 20:58:55 +0300

    feat: fix error

--- lib/data/model/suggested_word.dart
+++ lib/data/model/suggested_word.dart
@@ -1,5 +1,6 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
 part 'suggested_word.freezed.dart';
 part 'suggested_word.g.dart';
 
@@ -9,8 +10,8 @@ class SuggestedWord with _$SuggestedWord {
   const factory SuggestedWord({
     int? id,
     int? categoryId,
-    String? eng,
-    String? tr,
+    String? ENG,
+    String? TR,
   }) = _SuggestedWord;
 
   factory SuggestedWord.fromJson(Map<String, dynamic> json) =>
