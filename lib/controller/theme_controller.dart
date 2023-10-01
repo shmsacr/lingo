ae71be0e36eb93c6e6066939c17c74193615874f
shmsacr <shmsacr@gmail.com>
2023-10-01 21:44:32 +0300

    feat: delete theme ct

--- lib/controller/theme_controller.dart
+++ lib/controller/theme_controller.dart
@@ -1,13 +1,11 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final themeProvider = StateNotifierProvider<ThemeNotifier, bool>((ref) {
-  return ThemeNotifier();
-});
-
-class ThemeNotifier extends StateNotifier<bool> {
-  ThemeNotifier() : super(false);
-
-  void toggleTheme() {
-    state = !state;
-  }
-}
+// final themeProvider = StateNotifierProvider<ThemeNotifier, bool>((ref) {
+//   return ThemeNotifier();
+// });
+//
+// class ThemeNotifier extends StateNotifier<bool> {
+//   ThemeNotifier() : super(false);
+//
+//   void toggleTheme() {
+//     state = !state;
+//   }
+// }
